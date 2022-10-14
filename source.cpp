b3cae6498dfdc5057bc0e1c5560c374ecb86d987
LazyHat <105165429+LazyHat@users.noreply.github.com>
2022-10-14 15:24:56 +0300

    Create a struct

--- source.cpp
+++ source.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <ifstream>
+#include <ofstream>
+#include "Node.h"
 using namespace std;
-    
+
+ofstream fo;
 
 int main(){
     cout << "Hello World\n";
+    fo.open("tree.txt");
 }
