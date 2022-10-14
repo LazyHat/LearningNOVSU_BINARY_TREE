1761bee3f00652f2808881c46385213b67ad87fe
LazyHat <105165429+LazyHat@users.noreply.github.com>
2022-10-14 18:21:38 +0300

    Develop struct, begin()

--- Node.h
+++ Node.h
@@ -1,4 +1,29 @@
+#include <string>
+#include <vector>
+using namespace std;
+typedef vector<int> intvec;
 struct Node
 {
-    
+    intvec coordinates;
+    bool end = false;
+    string question = "Question not exist\n";
+    Node *No;
+    Node *Yes;
+    Node(string Question)
+    {
+        question = Question + "\n";
+    }
+    Node(string Question, bool yesno)
+    {
+        coordinates = this->coordinates;
+        coordinates.push_back(yesno);
+        question = Question + "\n";
+    }
+    Node(bool isEnd, string Answer, bool yesno)
+    {
+        coordinates = this->coordinates;
+        coordinates.push_back(yesno);
+        end = true;
+        question = Answer + "\n";
+    }
 };
--- source.cpp
+++ source.cpp
@@ -1,12 +1,48 @@
 #include <iostream>
-#include <ifstream>
-#include <ofstream>
+#include <fstream>
+#include <clocale>
 #include "Node.h"
 using namespace std;
 
 ofstream fo;
+string VectorToStr(intvec &v)
+{
+    string s;
+    for (int i = 0; i < v.size(); i++)
+    {
+        s += v[i];
+    }
+    return s;
+}
+void Begin(Node *firstNode)
+{
+    char answer;
+    Node node = *firstNode;
+    do
+    {
+        cout << node.question << "Your Answer(y/n): ";
+        cin >> answer;
+        if (answer == 'y')
+        {
+            node = *(node.Yes);
+        }
+        else if (answer == 'n')
+        {
+            node = *(node.No);
+        }
+    } while (!node.end);
+    if (node.end)
+    {
+        cout << node.question;
+    }
+}
 
-int main(){
+int main()
+{
+    setlocale(LC_ALL, "Russian");
     cout << "Hello World\n";
-    fo.open("tree.txt");
+    Node first = Node("It is a cat?");
+    first.Yes = new Node(true, "I WIN NAHUI!", true);
+    first.No = new Node(true, "fuck.", false);
+    Begin(&first);
 }
