42e85dc788f942f6e3a3ef9e1fbe389d2bb99af6
LazyHat <105165429+LazyHat@users.noreply.github.com>
2022-10-17 08:37:04 +0300

    апвенгов

--- Node.h
+++ Node.h
@@ -1,26 +1,13 @@
 #include <string>
-#include <vector>
 using namespace std;
 struct Node
 {
-    string coordinates;
     bool end = false;
-    string question = "Question not exist\n";
-    Node *No;
-    Node *Yes;
-    Node(string Question)
+    string question = "Question not exist";
+    Node(string Question_or_Answer)
     {
-        question = Question + "\n";
+        question = Question_or_Answer;
+        end = !Question_or_Answer.ends_with('?');
     }
-    Node(string Question, string yesno)
-    {
-        coordinates = this->coordinates + yesno;
-        question = Question + "\n";
-    }
-    Node(bool isEnd, string Answer, string yesno)
-    {
-        coordinates = this->coordinates + yesno;
-        end = true;
-        question = Answer + "\n";
-    }
+    Node() {}
 };
--- source.cpp
+++ source.cpp
@@ -1,12 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <iterator>
-#include <clocale>
 #include <list>
+#include <map>
 #include "Node.h"
 using namespace std;
-
+typedef map<string, Node> Dic;
+#pragma region OBJECTS
+ofstream fo;
+Dic db;
+#pragma endregion
 #pragma region FUNCTIONS
+#pragma region PROTOTYPES
+#pragma endregion
+#pragma region SETTERS_AND_GETTERS
+void AddToDic(string Coordinates, Node Object)
+{
+    db.insert(Dic::value_type(Coordinates, Object));
+}
+Node GetNodeFromDic(string Coordinates)
+{
+    if (db.contains(Coordinates))
+    {
+        return db.at(Coordinates);
+    }
+    else
+    {
+        return Node("0.Node whose coords are:" + Coordinates + " does not exist.");
+    }
+}
+void ReplaceItemDic(string Coordinates, Node Object)
+{
+    if (db.contains(Coordinates))
+    {
+        db[Coordinates] = Object;
+    }
+    else
+    {
+        AddToDic(Coordinates, Object);
+    }
+}
+#pragma endregion
 #pragma region PRINTS_AND_INPUTS
 void PrintS(string s)
 {
@@ -43,95 +77,220 @@ int InputInt()
 string InputStr()
 {
     string s;
-    cin >> s;
+    getline(cin >> ws, s);
     return s;
 }
-#pragma endregion
-string VectorToStr(vector<int> &v)
+char InputChar()
 {
-    string s;
-    for (int i = 0; i < v.size(); i++)
-    {
-        s += v[i];
-    }
-    return s;
+    char c;
+    cin >> c;
+    return c;
 }
-void EditNode(string coordinates)
+string InputChary_n()
 {
-    // Может сделать меню, выбор что поменять
+    string c;
+    cin >> c;
+    if (c == "da" || c == "net")
+        return c;
+    PrintSL("Incorrect answer, enter correct answer!");
+    return InputChary_n();
 }
-void CreateNode()
+int PrintMenuAndChoose(string head, list<string> ListMenu)
 {
-    //
-}
-string PrintMenuFromList(list<string> ListMenu)
-{
+    PrintSL("");
     PrintS("-", 5);
-    PrintS("MENU");
+    PrintS(head);
     PrintSL("-", 5);
     // Menu
-    copy(ListMenu.begin(), ListMenu.end(), ostream_iterator<string>(cout, "\n"));
-    // EndMenu
-    PrintSL("-", 14);
-    PrintSL("Choose: ");
+    {
+        int j = 1;
+        for (auto &&i : ListMenu)
+        {
+            PrintSL(to_string(j) + "." + i);
+            j++;
+        }
+    }
+    //  EndMenu
+    PrintSL("-", 10 + head.size());
+    PrintS("Choose: ");
+    return InputInt();
 }
-void Begin()
+void PrintMenu(string head, list<string> ListMenu)
 {
-    char answer;
-    Node node = first;
+    PrintSL("");
+    PrintS("-", 5);
+    PrintS(head);
+    PrintSL("-", 5);
+    // Menu
+    {
+        int j = 1;
+        for (auto &&i : ListMenu)
+        {
+            PrintSL(to_string(j) + "." + i);
+            j++;
+        }
+    }
+    //  EndMenu
+    PrintSL("-", 10 + head.size());
+}
+#pragma endregion
+#pragma region CONVERT
+#pragma endregion
+#pragma region EDITTREE
+void EditNode(string coordinates)
+{
+    string NextCoords;
+    Node currentnode = GetNodeFromDic(coordinates);
+    bool k = true;
     do
     {
-        cout << node.question << "Your Answer(y/n): ";
-        cin >> answer;
-        if (answer == 'y')
+        string q_a = (currentnode.end ? "Answer(P.S. or enter new Question)" : "Question(P.S. or enter new Answer)");
+        list<string> menulist = {"Type of Node: " + q_a,
+                                 q_a + ": " + currentnode.question};
+        list<string> chooselist = {"Edit " + q_a,
+                                   "Go up the tree",
+                                   "Exit Menu"};
+        if (!currentnode.end)
         {
-            node = *(node.Yes);
+            chooselist.push_back("Go to Yes");
+            chooselist.push_back("Go to No");
+            menulist.push_back("If yes: " + GetNodeFromDic(coordinates + "1").question);
+            menulist.push_back("If No: " + GetNodeFromDic(coordinates + "0").question);
         }
-        else if (answer == 'n')
+        PrintMenu("ThisNode(" + coordinates + ")", menulist);
+        // Может сделать меню, выбор что поменять
+        switch (PrintMenuAndChoose("EditNodeMenu", chooselist))
         {
-            node = *(node.No);
+        case 1:
+            PrintS("Enter new " + q_a + ": ");
+            currentnode = Node(InputStr());
+            break;
+        case 2:
+            if (currentnode.end)
+            {
+                string _coords = coordinates;
+                _coords.pop_back();
+                NextCoords = _coords;
+                k = false;
+            }
+            else
+            {
+                k = false;
+                NextCoords = coordinates + "1";
+            }
+            break;
+        case 3:
+            if (currentnode.end)
+            {
+                k = false;
+            }
+            else
+            {
+                k = false;
+                NextCoords = coordinates + "0";
+            }
+            break;
+        case 4:
+        {
+            if (currentnode.end)
+            {
+                PrintSL("Exist item");
+                NextCoords = coordinates;
+                k = false;
+            }
+            else
+            {
+                string _coords = coordinates;
+                _coords.pop_back();
+                NextCoords = _coords;
+                k = false;
+            }
         }
-    } while (!node.end);
-    if (node.end)
+        break;
+        case 5:
+            if (currentnode.end)
+            {
+                PrintSL("Exist item");
+                NextCoords = coordinates;
+                k = false;
+            }
+            else
+            {
+                k = false;
+            }
+            break;
+        default:
+            PrintSL("Exist item");
+            k = false;
+            NextCoords = coordinates;
+            break;
+        }
+    } while (k);
+    ReplaceItemDic(coordinates, currentnode);
+    if (NextCoords != "")
     {
-        cout << node.question;
+        EditNode(NextCoords);
     }
 }
+#pragma endregion
+void Begin()
+{
+    string answer;
+    Node node = GetNodeFromDic(".");
+    string coordinates = ".";
+    while (!node.end)
+    {
+        PrintS(node.question + "(da/net): ");
+        answer = InputChary_n();
+        if (answer == "da")
+        {
+            coordinates += "1";
+        }
+        else if (answer == "net") // answer = 'n'
+        {
+            coordinates += "0";
+        }
+        node = GetNodeFromDic(coordinates);
+    }
+    PrintS("Your animal: " + node.question + "\nIts a correct answer?(da/net): ");
+    answer = InputChary_n();
+    if (answer == "da")
+    {
+        PrintSL("I win");
+    }
+    else if (answer == "net")
+    {
+        PrintS("Are you want to edit this node?(da/net): ");
+        if (InputChary_n() == "da")
+        {
+            EditNode(coordinates);
+        }
+    }
+}
+//надо сделать чтобы не координату вводить,
+//а как то связать с игрой, чтобы можно было добавить узел прямо в игре. сделать типо мастера создания узлов.
 void HeadMenu()
 {
-    PrintMenuFromList({"1.Play a game", "2.Edit tree", "3.Create tree"});
-    switch (InputInt())
+    switch (PrintMenuAndChoose("MENU",
+                               {"Play a game",
+                                "Edit tree"}))
     {
-    case 1:
+    case 1: // Play a game
         Begin();
         break;
-    case 2:
-        PrintSL("Enter coordinates, of Node: ");
+    case 2: // Edit tree
+        PrintS("Enter coordinates of node: ");
         EditNode(InputStr());
-        break;
-    case 3:
-        PrintSL("Enter coordinates, of Node: ");
-        CreateNode(InputStr()); // надо сделать чтобы не координату вводить. а как то связать с игрой, чтобы можно было добавить узел прямо в игре. сделать типо мастера создания узлов
         break;
     default:
         break;
     }
 }
-
-#pragma endregion
-
-#pragma region OBJECTS
-ofstream fo;
-Node first;
 #pragma endregion
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
     cout << "Hello World\n";
-    first = Node("QuestFirst?");
-    first.Yes = new Node(true, "Quest1", "1");
-    first.No = new Node(true, "Quest0", "0");
     while (true)
         HeadMenu();
 }
